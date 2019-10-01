2487b513b69468b422ecdb25d791b0da957fa2cd
Ketut Sandiarsa <ktutnik@gmail.com>
2019-10-01 12:08:29 +0800

    Revert "Create auth controller"

--- packages/server/src/config.d.ts
+++ packages/server/src/config.d.ts
@@ -1,13 +1,6 @@
 declare namespace NodeJS {
     export interface ProcessEnv {
-        PORT: string 
+        PORT: string, 
         MONGODB_URI:string
-        JWT_SECRET: string,
-        FACEBOOK_CLIENT_ID:string
-        FACEBOOK_SECRET:string
-        GOOGLE_CLIENT_ID:string
-        GOOGLE_SECRET:string
-        GITHUB_CLIENT_ID:string
-        GITHUB_SECRET:string
     }
 }
