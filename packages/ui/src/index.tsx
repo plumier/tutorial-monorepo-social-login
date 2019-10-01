9f4b60ea3bb638ac0d8184c1ea18b3e8758ebdb2
Hantama <49479929+Hantama237@users.noreply.github.com>
2019-10-01 12:13:30 +0800

    login-ui (#11)
    create the login ui

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Login from './page/LoginPage';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Login />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ packages/ui/src/page/LoginPage.tsx
@@ -0,0 +1,36 @@
+import "../style/Login.css"
+import React from "react"
+
+function Login(){
+    function Login(){
+
+    }
+    return(
+        <div className="container">
+            <div className="center-container">
+                <div className="form-container">
+                    <h2>Login to To-Do</h2>
+                    <input className="input-form" type="text" placeholder="E-mail"/><br/>
+                    <input className="input-form" type="password" placeholder="Password"/><br/>
+                    <button className="login">Login</button><br/>
+                    <button className="login-google">
+                        Login with
+                        <span className="text-google">
+                            <span className="blue"> G</span>
+                            <span className="red">o</span>
+                            <span className="yellow">o</span>
+                            <span className="blue">g</span>
+                            <span className="green">l</span>
+                            <span className="red">e</span>
+                        </span>
+                    </button><br/>
+                    <button className="login-social">Login with <span className="text-facebook">Facebook</span></button><br/>
+                    <button className="login-social">Login with <span className="text-github">Github</span></button><br/>
+
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Login;
