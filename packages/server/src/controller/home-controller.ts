8b28e7498a5d4c9c1cf22b4a0aba0d918db7515b
Ketut Sandiarsa <ktutnik@gmail.com>
2019-11-11 16:10:28 +0800

    Fixing missing root url

--- packages/server/src/controller/home-controller.ts
+++ packages/server/src/controller/home-controller.ts
@@ -8,6 +8,7 @@ export class HomeController {
     //provide server side route to handle react route.
     //thus when user refresh browser in specific route, it will keep in the correct url
     @authorize.public()
+    @route.get("/")
     @route.historyApiFallback()
     index(){
         return response.file(join(__dirname, "../../../ui/build/index.html"))
